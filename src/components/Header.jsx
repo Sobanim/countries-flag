86087ae9421ea59df73bc94060327f79be9cb047
Sobanim <sobolev.dmirtiy@gmail.com>
2023-09-21 21:22:51 +0200

    Start working

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components'
+import {Container} from "./Containter";
+
+const Header = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
