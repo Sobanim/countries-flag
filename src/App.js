c5e86d94c623e087a5aec2966b2d054ed5ef61f9
Sobanim <sobolev.dmirtiy@gmail.com>
2023-10-16 21:27:14 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <>
+      <Header />
+    </>
 
-    </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
+
+import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 import {Container} from './Containter'
 
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background: (--colors-ui-base);
+  
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`
+
+const Title = styled.a.attrs({
+  href: '/'
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`
+
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  //font-weight: var(--fw-bold);
+  text-transform: capitalize;
+`
 const Header = () => {
+  const [theme, setTheme] = useState('light')
+
+  const toogleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
   return (
-    <>
-      <Container/>
-      <div>
-          123213
-          asd
-
-      </div>
-    </>
-
+    <HeaderEl>
+      <Container>
+        <Wrapper>
+          <Title>Where is the world?</Title>
+          <ModeSwitcher onClick={toogleTheme}>
+            { theme === 'light' ? (
+              <IoMoonOutline size={'14px'} style={{marginRight: '0.75rem'}}/>
+            ) : (
+              <IoMoon size={'14px'} style={{marginRight: '0.75rem'}}/>
+            )}
+            <span style={{marginLeft: '0.75rem'}}>{theme} theme</span>
+          </ModeSwitcher>
+        </Wrapper>
+      </Container>
+    </HeaderEl>
   )
 }
 
