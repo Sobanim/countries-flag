e0dc8c321edce404191bdd9d3e2f22db32496e2b
Sobanim <sobolev.dmirtiy@gmail.com>
2024-01-17 23:04:20 +0100

    Working on detail component

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useNavigate, useParams} from 'react-router-dom'
+import {IoArrowBack} from 'react-icons/io5'
+import axios from 'axios'
+import {searchByCountry} from '../config'
+import {Button} from '../components/Button'
+import Info from '../components/Info'
 
-const Details = ({match}) => {
-  console.log('match', match)
+
+const Details = () => {
+  const {name} = useParams()
+  const {push, goBack} = useNavigate()
+
+  const [country, setCountry] = useState(null)
+  console.log('country', country)
+
+  useEffect(() => {
+    axios.get(searchByCountry(name))
+      .then(({data}) => setCountry(data[0]))
+  }, [name])
+
   return (
     <div>
-            Details
+      <Button onClick={goBack}>
+        <IoArrowBack />Back
+      </Button>
+            Details {name}
+      {/*{ country ? <Info {...country} /> : null }*/}
     </div>
   )
 }
